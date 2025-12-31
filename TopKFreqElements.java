850c0db1d2d9972a2821c566eb898927ae0e7d94
Jerry Dobner <jdobner@users.noreply.github.com>
2025-12-31 11:22:51 -0500

    .

--- TopKFreqElements.java
+++ TopKFreqElements.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.NavigableSet;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -7,45 +8,51 @@ public class TopKFreqElements {
         public int[] topKFrequent(int[] nums, int k) {
             Arrays.sort(nums);
 
-            int current = nums[0] + 1;
+            int current = 0;
             int count = 0;
-            final var sorted = new TreeSet<Elem>();
-            for (int i : nums) {
-                if (i != current) {
-                    if (count > 0) {
-                        var e = new Elem();
-                        e.count = count;
-                        e.value = current;
-                        sorted.add(e);
-                        if (sorted.size() > k) {
-                            sorted.remove(sorted.last());
-                        }
+            final NavigableSet<Elem> sorted = new TreeSet<Elem>();
+            var i = 0;
+            do {
+                current = nums[i++];
+                count++;
+                if (i == nums.length || nums[i] != current) {
+                    var e = new Elem();
+                    e.count = count;
+                    e.value = current;
+                    sorted.add(e);
+                    if (sorted.size() > k) {
+                        sorted.pollFirst();
                     }
-                    count = 1;
-                    current = i;
-                } else {
-                    count++;
+                    count = 0;
                 }
+            } while (i < nums.length);
+            i = 0;
+            var result = new int[sorted.size()];
+            for (Elem e : sorted) {
+                result[i++] = e.value;
             }
-            return sort;
+            return result;
         }
+        class Elem implements Comparable<Elem> {
+            public int count;
+            public int value;
 
-        
+            @Override
+            public int compareTo(Elem e) {
+                int rv = Integer.compare(this.count, e.count);
+                if (rv == 0) {
+                    rv = Integer.compare(this.value, e.value);
+                }
+                return rv;
+            }
+        }
     }
 
-    public static class Elem implements Comparable<Elem> {
-        public int count;
-        public int value;
-
-        @Override
-        public int compareTo(Elem e) {
-            int rv = this.value - e.value;
-            if (rv == 0) {
-                rv = this.value - e.value;
-            }
-            return rv;
-        }
-
+    public static void main(String[] args) {
+        var s = new TopKFreqElements().new Solution();
+        System.out.println(Arrays.toString(s.topKFrequent(new int[] { 1, 1, 1, 2, 2, 3 }, 2)));
+        System.out.println(Arrays.toString(s.topKFrequent(new int[] { 1 }, 1)));
+        System.out.println(Arrays.toString(s.topKFrequent(new int[] { -1, -1 }, 1)));
 
     }
 }

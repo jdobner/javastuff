6b59828ebc1c5c8ba6fa7dd18795dd65476bc775
Jerry Dobner <jdobner@users.noreply.github.com>
2026-02-19 11:57:51 -0500

    5ms!! beats 98.36% see https://leetcode.com/problems/print-zero-even-odd/submissions/1924600333/

--- ZeroEvenOdd.java
+++ ZeroEvenOdd.java
@@ -1,130 +1,103 @@
 import java.util.function.IntConsumer;
 
-
 class ZeroEvenOdd {
     private final int n;
     private boolean lastIsZero = false;
-    private int last = 0;
-    private final int maxEven;  
-    private final int maxOdd; 
+    private int lastNumber = 0;
     private final java.io.PrintStream o = System.out;
-    
+
     public ZeroEvenOdd(int n) {
         this.n = n;
-        this.maxOdd = n % 2 == 1 ? n : n - 1;
-        this.maxEven = n % 2 == 0 ? n : n - 1;
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
-    public void zero(IntConsumer printNumber) throws InterruptedException {                
-        o.println("BS zero");
-        synchronized(this) {
-            while (last < n) {
-                while (lastIsZero) {
-                    o.println("W zero");
-                    this.wait();
-                }
-                // if (last == n) {
-                //     this.notifyAll();
-                //     return;
-                // }
-                if(last < n) {
-                    o.println("P zero");
-                    printNumber.accept(0);
-                    lastIsZero = true;
-                    this.notifyAll();
-                }
+    public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
+        // o.println("BS zero");
+        while (lastNumber < n) {
+            while (lastIsZero) {
+                // o.println("W zero");
+                this.wait();
             }
-            o.println("D zero");
-        }
-    }
-
-    public void even(IntConsumer printNumber) throws InterruptedException {                
-        o.println("BS even");
-        synchronized(this) {
-            while (last < maxEven) {
-                while (!lastIsZero || this.last % 2 == 0) {
-                    o.println("W even");
-                    this.wait();
-                }
-                // if (last == n) {
-                //     this.notifyAll();
-                //     return;
-                // }
-                o.println("P even");
-                printNumber.accept(++last);
-                lastIsZero = false;
+            if (lastNumber < n) {
+                // o.println("P zero");
+                printNumber.accept(0);
+                lastIsZero = true;
                 this.notifyAll();
             }
-            o.println("D even");
         }
+        // o.println("D zero");
     }
 
-    public void odd(IntConsumer printNumber) throws InterruptedException {
-        o.println("BS odd");
-        synchronized(this) {
-            while (last < maxOdd) {
-                while (!lastIsZero || this.last % 2 == 1) {
-                    o.println("W odd");
-                    this.wait();
-                }
-                o.println("P odd");
-                printNumber.accept(++last);
-                lastIsZero = false;
-                this.notifyAll();
+    public synchronized void even(IntConsumer printNumber) throws InterruptedException {
+        // o.println("BS even");
+        var max = n % 2 == 0 ? n : n - 1;
+        while (lastNumber < max) {
+            while (!lastIsZero || this.lastNumber % 2 == 0) {
+                // o.println("W even");
+                this.wait();
             }
-            o.println("D odd");
+            // o.println("P even");
+            printNumber.accept(++lastNumber);
+            lastIsZero = false;
+            this.notifyAll();
         }
+        // o.println("D even");
     }
 
-    
-    
-    public static void main(String[] args) {
-        ZeroEvenOdd x = new ZeroEvenOdd(7);
-        
-        var p = new IntConsumer() {
-            StringBuilder sb = new StringBuilder();
-            @Override
-            public void accept(int value) {
-                sb.append(value);
+    public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
+        // o.println("BS odd");
+        var max = n % 2 == 1 ? n : n - 1;
+        while (lastNumber < max) {
+            while (!lastIsZero || this.lastNumber % 2 == 1) {
+                // o.println("W odd");
+                this.wait();
+            }
+            // o.println("P odd");
+            printNumber.accept(++lastNumber);
+            lastIsZero = false;
+            this.notifyAll();
+        }
+        // o.println("D odd");
+    }
+
+    private interface Iterruptable {
+        public void run() throws InterruptedException;
+    }
+
+    @FunctionalInterface
+    interface InterruptibleRunnable {
+        void run() throws InterruptedException;
+    }
+
+    static Runnable wrap(InterruptibleRunnable r) {
+        return () -> {
+            try {
+                r.run();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return; // clean exit
             }
         };
+    }
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    x.zero(p);
-                } catch(Exception e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println(p.sb);
-            }       
-       }).start();
+    public static void main(String[] args) throws Exception {
+        final ZeroEvenOdd x = new ZeroEvenOdd(201);
+        final var sb = new StringBuilder();
+        final IntConsumer p = sb::append;
 
-       new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    x.odd(p);
-                } catch(Exception e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println(p.sb);
-            }       
-       }).start();
+        var zero = new Thread(wrap(() -> x.zero(p)), "zero");
+        var odd = new Thread(wrap(() -> x.odd(p)), "odd");
+        var even = new Thread(wrap(() -> x.even(p)), "even");
 
-       new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    x.even(p);
-                } catch(Exception e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println(p.sb);
-            }       
-       }).start();
+        zero.start();
+        odd.start();
+        even.start();
+
+        zero.join();
+        odd.join();
+        even.join();
+
+        System.out.println(sb);
 
     }
 }
